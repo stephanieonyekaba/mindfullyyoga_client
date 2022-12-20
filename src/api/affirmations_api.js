f662db34594d80cd2859a9f2d4feaf8510647209
stephanieonyekaba <onyek009@umn.edu>
2022-12-19 20:24:54 -0700

    m

--- src/api/affirmations_api.js
+++ src/api/affirmations_api.js
@@ -2,6 +2,7 @@ import apiUrl from '../apiConfig'
 import axios from 'axios'
 
 //note
+//ffff
 //index function 
 export const getAllAffirmations = () => {
     return axios(`${apiUrl}/affirmations`)
